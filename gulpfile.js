3773c7f0bdfd292dafa340809672eb83e3c0de94
xwartz <stddup@gmail.com>
2015-11-27 00:14:16 +0800

    Firefox extension

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,13 @@
 var gulp = require('gulp');
 var zip = require('gulp-zip');
 var plumber = require('gulp-plumber');
+var shell = require('gulp-shell');
 
 
 var dirs = {
   'dest': './',
-  'chrome': './chrome'
+  'chrome': './chrome',
+  'firefox': './firefox'
 };
 
 gulp.task('chrome', function() {
@@ -14,3 +16,16 @@ gulp.task('chrome', function() {
     .pipe(zip('chrome.zip'))
     .pipe(gulp.dest(dirs.dest))
 });
+
+gulp.task('firefox', function() {
+  gulp.src(dirs.firefox + '/**')
+    .pipe(plumber())
+    .pipe(zip('firefox.crx'))
+    .pipe(gulp.dest(dirs.dest))
+});
+
+
+gulp.task('pkg', shell.task([
+  'gulp chrome',
+  'gulp firefox'
+]))
