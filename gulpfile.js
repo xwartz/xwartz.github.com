78e79d9621522d281a854d20f2b9759a46fdaf68
xwartz <stddup@gmail.com>
2015-11-27 10:08:46 +0800

    html 分成模板

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,15 @@ var gulp = require('gulp');
 var zip = require('gulp-zip');
 var plumber = require('gulp-plumber');
 var shell = require('gulp-shell');
+var notify = require('gulp-notify');
+var handlebars = require('gulp-compile-handlebars');
 
+
+var plumberErrorHandler = {
+    errorHandler: notify.onError({
+        message: "Error: <%= error.message %>"
+    })
+};
 
 var dirs = {
   'dest': './',
@@ -28,4 +36,17 @@ gulp.task('firefox', function() {
 gulp.task('pkg', shell.task([
   'gulp chrome',
   'gulp firefox'
-]))
+]));
+
+var hbs = function() {
+  var data = {}
+  var options = {
+    batch: [dirs.hbs]
+  }
+
+  return gulp.src([dirs.entry + '/*.hbs'])
+    .pipe(plumber(plumberErrorHandler))
+    .pipe(handlebars(data, options))
+    .pipe(gulp.dest(dirs.dest))
+
+}
